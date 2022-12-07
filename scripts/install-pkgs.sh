fea4186adb62137f2a2783b927c8107d6af99ba1
unilock <unilock@fennet.rentals>
2022-12-07 09:53:22 -0500

    homebrew formulae --head
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/install-pkgs.sh
+++ scripts/install-pkgs.sh
@@ -7,6 +7,9 @@ IFS=$'\n'
 print_line "Installing Homebrew formulae."
 cat lists/brew-formulae | xargs brew install --formula
 
+print_line "Installing Homebrew formulae. (--HEAD)"
+cat lists/brew-head-packages | xargs brew install --HEAD
+
 print_line "Installing Homebrew casks."
 cat lists/brew-casks | xargs brew install --cask
 
