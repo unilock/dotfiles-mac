dfff4e61b589cee02bf34746b867a118fdad792c
unilock <unilock@fennet.rentals>
2023-01-11 15:42:53 -0500

    new macOS defaults .DS_Store go away
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/defaults-write.sh
+++ scripts/defaults-write.sh
@@ -3,4 +3,7 @@ source functions/common
 
 print_line "Setting macOS defaults."
 
+print_line ".DS_Store go away (on network volumes)"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
 # ...
