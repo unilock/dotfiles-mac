afd888f03cfe7de1dc830050609bbbf7e1d66f19
unilock <unilock@fennet.rentals>
2024-09-17 15:55:13 -0400

    updates
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/defaults-write.sh
+++ scripts/defaults-write.sh
@@ -3,7 +3,13 @@ source functions/common
 
 print_line "Setting macOS defaults."
 
-print_line ".DS_Store go away (on network volumes)"
+print_line "Don't write .DS_Store to network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+print_line "Don't write .DS_Store to removable volumes"
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+print_line "Disable dock resizing"
+defaults write com.apple.Dock size-immutable -bool true
 
 # ...
--- scripts/install-extra-pkgs.sh
+++ scripts/install-extra-pkgs.sh
@@ -6,14 +6,20 @@ source functions/common
 print_line "Installing IDEA wrapper."
 echo 'open -na "IntelliJ IDEA CE.app" --args "$@"' > "$HOME/.local/bin/idea"
 
-print_line "Installing lsusb."
-curl -o"$HOME/.local/bin/lsusb" https://raw.githubusercontent.com/jlhonora/lsusb/master/lsusb
+print_line "Installing jargrep."
+curl -L -o"$HOME/.local/jargrep-1.0-all.jar" https://github.com/CrackedPolishedBlackstoneBricksMC/jargrep/releases/download/1.0/jargrep-1.0-all.jar
+echo 'java -jar $HOME/.local/jargrep-1.0-all.jar "$@"' > "$HOME/.local/bin/jargrep"
 
-# surely this is safe
+print_line "Installing unbted."
+curl -L -o"$HOME/.local/unbted-1.2.1.jar" https://git.sleeping.town/unascribed/unbted/releases/download/v1.2.1/unbted-1.2.1.jar
+echo 'java -jar $HOME/.local/unbted-1.2.1.jar "$@"' > "$HOME/.local/bin/unbted"
+
 print_line "Installing Vineflower."
-curl -L -o"$HOME/.local/vineflower.jar" https://github.com/Vineflower/vineflower/releases/download/1.9.1/vineflower-1.9.1.jar
-echo 'java -Xms4G -Xmx4G -jar $HOME/.local/vineflower.jar "$@"' > "$HOME/.local/bin/vineflower"
+curl -L -o"$HOME/.local/vineflower-1.10.1.jar" https://github.com/Vineflower/vineflower/releases/download/1.10.1/vineflower-1.10.1.jar
+echo 'java -Xms512M -Xmx4096M -jar $HOME/.local/vineflower-1.10.1.jar --indent-string="    " --thread-count=4 "$@"' > "$HOME/.local/bin/vineflower"
 
 chmod +x "$HOME/.local/bin/idea"
 chmod +x "$HOME/.local/bin/lsusb"
+chmod +x "$HOME/.local/bin/jargrep"
+chmod +x "$HOME/.local/bin/unbted"
 chmod +x "$HOME/.local/bin/vineflower"
