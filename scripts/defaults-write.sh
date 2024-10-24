d54ec00bebd4e12de4096c3481ce6bd16b163e48
unilock <unilock@fennet.rentals>
2024-10-24 14:37:28 -0400

    more defaults
    Signed-off-by: unilock <unilock@fennet.rentals>

--- scripts/defaults-write.sh
+++ scripts/defaults-write.sh
@@ -9,7 +9,17 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 print_line "Don't write .DS_Store to removable volumes"
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-print_line "Disable dock resizing"
-defaults write com.apple.Dock size-immutable -bool true
+print_line "Disable Dock resizing"
+defaults write com.apple.dock size-immutable -bool true
+
+print_line "Set Dock size"
+defaults write com.apple.dock tilesize -int 36
+
+print_line "Disable press-and-hold for accented characters"
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+print_line "Set key repeat rate"
+defaults write -g InitialKeyRepeat -int 25
+defaults write -g KeyRepeat -int 2
 
 # ...
